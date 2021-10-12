9a02055066513d9ccc45722a451ed02af0982154
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-10-12 20:48:01 +1030

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#define CURL_STATICLIB
+#include <curl/curl.h>
+
 #include <iostream>
 #include <filesystem>
 
@@ -11,6 +14,59 @@ int main()
     std::cout << "=======================================" << "\n\n";
 }
 
+class Common_Class
+{       
+    public:
+        static size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream)
+        {
+            size_t written = fwrite(ptr, size, nmemb, (FILE*)stream);
+            return written;
+        }
+        void extract_serverini_file()
+        {
+            // Function uses: <iostream>, <string>, <filesystem>
+
+            const char* av_url = "http://osce14-p.activeupdate.trendmicro.com/activeupdate/server.ini";
+            // Converting std::string to char for downloading purposes.
+            std::string temp_int_filepath = std::filesystem::current_path().string() + "\\temp.ini";
+            char temp_filename[FILENAME_MAX];
+            // https://stackoverflow.com/questions/41915130/initializing-an-array-of-characters-with-a-string-variable
+            strcpy(temp_filename, temp_int_filepath.c_str());
+            std::cout << "[!] Temp filename/location: " << temp_filename << "\n\n";
+            download_file(av_url, temp_filename);
+        }
+        void download_file(const char* url, const char* full_pathname)
+        {
+            // Function uses: <iostream>
+
+            std::cout << "[!] Downloading the following: " << "\n";
+            std::cout << url << "\n";
+            std::cout << "[!] Downloading to: " << "\n";
+            std::cout << full_pathname << "\n\n";
+
+            CURL* curl;
+            FILE* fp;
+            CURLcode res;
+            curl = curl_easy_init();
+            if (curl)
+            {
+                fp = fopen(full_pathname, "wb");
+                curl_easy_setopt(curl, CURLOPT_URL, url);
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+                res = curl_easy_perform(curl);
+                curl_easy_cleanup(curl);
+                if (res != CURLE_OK)
+                {
+                    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+                }
+                fclose(fp);
+            }
+        }
+
+};
+
 /*
 Project Plan and Design
 
@@ -21,5 +77,6 @@ Brief : Console application used to assist with constructing URLs used to downlo
 === Minimum Functions ===
 [-] Reimplement functions of Smart_Scan_Pattern_Extractor-URL_Builder. Organised as a class file.
 [-] Replicate functions of Smart_Scan_Pattern_Extractor-URL_Builder and target towards to "Virus Pattern(s)".
+[-] ApplyObject-oriented programming.
 
 */
